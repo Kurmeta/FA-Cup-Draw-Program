8accf034f3c40a81d1a32563146c332fd6ae8b3c
Owen Kurmeta <owenkurmeta@gmail.com>
2023-06-03 13:56:32 +0100

    UPDATE: Can now get teams from file

--- FACUP_V1.py
+++ FACUP_V1.py
@@ -1,9 +1,10 @@
 import random
 import time
 
-teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8"]
-draw_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-leagues = ["Premier League", "Premier League", "Championship", "Championship", "League One", "League One", "League Two", "League Two"]
+def read_teams_from_file(file_path):
+    with open(file_path, "r") as file:
+        teams = file.readlines()
+    return [team.strip() for team in teams]
 
 def generate_fixtures(selected_teams):
     fixtures = []
@@ -11,21 +12,21 @@ def generate_fixtures(selected_teams):
     for i in range(0, len(selected_teams), 2):
         home_team = selected_teams[i]
         away_team = selected_teams[i+1]
-        fixture = (home_team[1], away_team[1])
+        fixture = (home_team, away_team)
         fixtures.append(fixture)
 
     return fixtures
 
-def display_quarter_finals():
+def display_quarter_finals(selected_teams):
     print("Quarter-Finals Teams:")
-    for draw_number, team, league in zip(draw_numbers, teams, leagues):
+    for i, team in enumerate(selected_teams, start=1):
         time.sleep(0.5)
-        print(f"Pick: {draw_number} | Team: {team} | League: {league}")
+        print(f"Pick: {i} | Team: {team}")
 
 def display_selected_team(selected_team):
     time.sleep(0.5)
-    draw_number, team_name = selected_team
-    print(f"Pick: {draw_number} | Selected Team: {team_name}")
+    pick_number, team_name = selected_team
+    print(f"Pick: {pick_number} | Selected Team: {team_name}")
 
 def display_fixture(fixture):
     home_team, away_team = fixture
@@ -34,19 +35,19 @@ def display_fixture(fixture):
     print(f"Fixture: {home_team} vs {away_team}")
     print("------------------------------------------------")
 
-def ordinal(n):
-    suffix = "th"
-    if 11 <= (n % 100) <= 13:
-        suffix = "th"
-    else:
-        suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
-    return f"{n}{suffix}"
-
 def main():
-    display_quarter_finals()
+    # Read teams from file
+    file_path = "teams.txt"  # Specify the path to your file
+    all_teams = read_teams_from_file(file_path)
 
-    selected_teams = [(draw_numbers[i], teams[i]) for i in range(len(teams))]
-    random.shuffle(selected_teams)
+    # Randomly select eight teams
+    selected_teams = random.sample(all_teams, 8)
+
+    # Display the quarter-finals teams
+    display_quarter_finals(selected_teams)
+
+    # Add pick numbers to the selected teams
+    selected_teams = [(i + 1, team) for i, team in enumerate(selected_teams)]
 
     for i, selected_team in enumerate(selected_teams, start=1):
         display_selected_team(selected_team)
