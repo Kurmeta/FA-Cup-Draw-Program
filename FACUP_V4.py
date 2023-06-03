ae576f6bcb06e514a4fe6d901b26334643bfedf9
Owen Kurmeta <owenkurmeta@gmail.com>
2023-06-03 14:08:53 +0100

    UPDATE: Improved Layout

--- FACUP_V4.py
+++ FACUP_V4.py
@@ -20,18 +20,18 @@ def generate_fixtures(selected_teams):
 
 def display_quarter_finals(selected_teams):
     text = "Quarter-Finals Teams:\n"
-    for i, team in enumerate(selected_teams, start=1):
-        text += f"Pick: {i} | Team: {team}\n"
+    for team in selected_teams:
+        text += f"{team}\n"
     return text
 
 def display_selected_team(selected_team):
     pick_number, team_name = selected_team
-    text = f"Pick: {pick_number} | Selected Team: {team_name}\n"
+    text = f"Pick: {pick_number}\n"
     return text
 
 def display_fixture(fixture):
     home_team, away_team = fixture
-    text = f"Fixture: {home_team} vs {away_team}\n"
+    text = f"{home_team} vs {away_team}\n"
     return text
 
 def main():
@@ -45,30 +45,41 @@ def main():
     # Initialize Tkinter window
     window = tk.Tk()
     window.title("Team Selection and Fixtures")
-    window.geometry("400x400")
 
-    # Create text widget for displaying information
-    text_widget = tk.Text(window, height=20, width=40)
-    text_widget.pack()
+    # Create left frame for teams
+    left_frame = tk.Frame(window)
+    left_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+    # Create middle frame for pick numbers
+    middle_frame = tk.Frame(window)
+    middle_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+    # Create right frame for fixtures
+    right_frame = tk.Frame(window)
+    right_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+    # Create text widgets for each frame
+    teams_text = tk.Text(left_frame, height=10, width=20)
+    teams_text.pack()
+    pick_text = tk.Text(middle_frame, height=10, width=10)
+    pick_text.pack()
+    fixture_text = tk.Text(right_frame, height=10, width=30)
+    fixture_text.pack()
 
     # Display the quarter-finals teams
     quarter_finals_text = display_quarter_finals(selected_teams)
-    text_widget.insert(tk.END, quarter_finals_text)
-    text_widget.update()
+    teams_text.insert(tk.END, quarter_finals_text)
 
     # Add pick numbers to the selected teams
     selected_teams = [(i + 1, team) for i, team in enumerate(selected_teams)]
 
     for i, selected_team in enumerate(selected_teams, start=1):
         selected_text = display_selected_team(selected_team)
-        text_widget.insert(tk.END, selected_text)
-        text_widget.update()
+        pick_text.insert(tk.END, selected_text)
 
         if i % 2 == 0:
             fixture = (selected_teams[i - 2][1], selected_teams[i - 1][1])
-            fixture_text = display_fixture(fixture)
-            text_widget.insert(tk.END, fixture_text)
-            text_widget.update()
+            fixture_text.insert(tk.END, display_fixture(fixture))
 
         time.sleep(0.5)
 
